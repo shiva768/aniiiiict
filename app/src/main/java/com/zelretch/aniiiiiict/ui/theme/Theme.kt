1535461b333ff3e0564b41d1d9363618763d0d89
shiva768 <reika.github@zelretch.com>
2025-05-08 21:31:50 +0900

    ちょっとお洒落で気に入っていたけど、deprecatedなので諦める

--- app/src/main/java/com/zelretch/aniiiiiict/ui/theme/Theme.kt
+++ app/src/main/java/com/zelretch/aniiiiiict/ui/theme/Theme.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -42,8 +41,8 @@ fun AniiiiictTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            val controller = WindowCompat.getInsetsController(window, view)
+            controller.isAppearanceLightStatusBars = darkTheme
         }
     }
 
