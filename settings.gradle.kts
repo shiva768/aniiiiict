15e3be69e35ba331fd0c5ac7ad013679ac1c752f
shiva768 <reika.github@zelretch.com>
2025-08-12 11:18:51 +0900

    feat: Gradle設定ファイルを整理
    - プロジェクトルートのbuild.gradle.ktsから不要なbuildscriptブロックを削除
    - libs.versions.tomlの各種ライブラリのバージョンを統一
    - libs.versions.tomlのライブラリをカテゴリに再分類
    - app/build.gradle.ktsのdependenciesブロックをカテゴリに合わせて整理
    - settings.gradle.ktsから不要なリポジトリを削除
    - Secrets Gradle Pluginを導入し、APIキーの管理方法を改善

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,6 @@ pluginManagement {
         gradlePluginPortal()
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
         maven { url = uri("https://www.jitpack.io") }
-        
-        // KSP用のリポジトリを追加
-        maven { url = uri("https://androidx.dev/snapshots/builds") }
-        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
     }
 }
 @Suppress("UnstableApiUsage")
@@ -19,12 +15,8 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
         maven { url = uri("https://www.jitpack.io") }
-        
-        // KSP用のリポジトリを追加
-        maven { url = uri("https://androidx.dev/snapshots/builds") }
-        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
     }
 }
 
 rootProject.name = "Aniiiiiict"
-include(":app") 
+include(":app")
