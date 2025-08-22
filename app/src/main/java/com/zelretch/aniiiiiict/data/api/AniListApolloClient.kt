6e21143c83bb4091cf86ae4d38441503ead50bd2
shiva768 <reika.github@zelretch.com>
2025-08-22 15:55:08 +0900

    unused importを削除

--- app/src/main/java/com/zelretch/aniiiiiict/data/api/AniListApolloClient.kt
+++ app/src/main/java/com/zelretch/aniiiiiict/data/api/AniListApolloClient.kt
@@ -2,7 +2,6 @@ package com.zelretch.aniiiiiict.data.api
 
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.ApolloResponse
-import com.apollographql.apollo.api.Mutation
 import com.apollographql.apollo.api.Query
 import com.apollographql.apollo.cache.normalized.FetchPolicy
 import com.apollographql.apollo.cache.normalized.fetchPolicy
@@ -23,7 +22,7 @@ class AniListApolloClient @Inject constructor(
     }
 
     private val client by lazy {
-        ApolloClient.Builder().serverUrl(SERVER_URL).okHttpClient(OkHttpClient()).build()
+        ApolloClient.Builder().serverUrl(SERVER_URL).okHttpClient(okHttpClient).build()
     }
 
     suspend fun <D : Query.Data> executeQuery(
@@ -38,22 +37,6 @@ class AniListApolloClient @Inject constructor(
             throw e
         } catch (e: IOException) {
             Timber.e(e, "[AniListApolloClient][%s] GraphQLクエリの実行に失敗: %s", context, operation.name())
-            throw e
-        }
-    }
-
-    suspend fun <D : Mutation.Data> executeMutation(
-        operation: Mutation<D>,
-        context: String,
-        cachePolicy: FetchPolicy = FetchPolicy.NetworkOnly
-    ): ApolloResponse<D> {
-        try {
-            return client.mutation(operation).fetchPolicy(cachePolicy).execute()
-        } catch (e: ApolloException) {
-            Timber.e(e, "[AniListApolloClient][%s] GraphQLミューテーションの実行に失敗: %s", context, operation.name())
-            throw e
-        } catch (e: IOException) {
-            Timber.e(e, "[AniListApolloClient][%s] GraphQLミューテーションの実行に失敗: %s", context, operation.name())
             throw e
         }
     }
